024d74d8c22ae9630f6d90beba8d519dfdc58bc0
Jesse Brown <rudy@Craigs-MacBook-Pro.local>
2021-08-24 10:37:20 -0400

    First Commit: Object Passing.

---
+++ src/com/objectpassing/Passer.java
@@ -0,0 +1,15 @@
+package com.objectpassing;
+
+public class Passer
+{
+
+    public static void main (String [] stgs) {
+        
+        
+        
+       System.out.println("Object Passing");
+        
+    }
+    
+    
+}
